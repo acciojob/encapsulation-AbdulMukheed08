3207ad2d301b9abdbc464f4f15f56f5dac233e64
Abdul Mukheed <you@example.com>
2023-01-22 14:41:37 +0530

    Polymorphism 5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj = new RWOnly();
+      /*try{
+        obj.name = "Abdul";
+      }
+      catch(Exception e){
+        System.out.println(e);
+    }*/
+      //name has private access in com.driver.RWOnly
+      obj.setName("Abdul");
+      System.out.println(obj.getName());
+  }
 }
